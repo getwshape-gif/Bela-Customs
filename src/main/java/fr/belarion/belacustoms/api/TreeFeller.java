b4414db80d0a831c9d742113039bfa24b5d63a9c
getwshape-gif <getwshape@gmail.com>
2026-07-20 10:25:11 -0700

    Delete src/main directory

--- src/main/java/fr/belarion/belacustoms/api/TreeFeller.java
+++
@@ -1,9 +0,0 @@
-package fr.belarion.belacustoms.api;
-
-/**
- * Marqueur pour la hache emeraude renforcee : casser une seule buche coupe
- * l'integralite de l'arbre (tree capitator). Consommee par le listener de
- * minage des custom items.
- */
-public interface TreeFeller {
-}
